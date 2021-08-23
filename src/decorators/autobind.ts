2ef396eef780a9bcdbdfa92c66e9e5421314a753
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-08-23 19:33:16 +0300

    Splitted to different files and folders

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,15 @@
+// Autobind 'this' decorator
+namespace App {
+  export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      enumerable: false,
+      get() {
+        return originalMethod.bind(this);
+      },
+    };
+
+    return adjDescriptor;
+  }
+}
